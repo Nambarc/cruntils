ed6c454716623a86551ff3dbd99e9cc23794c6fc
Nambarc <25438361+Nambarc@users.noreply.github.com>
2023-09-08 12:55:05 +0100

    Couple more utilities
    Introduced some constants.
    Deleted duplicates.
    Couple more utilities.

--- cruntils/utils.py
+++ cruntils/utils.py
@@ -1,6 +1,14 @@
 
 import math
 
+#------------------------------------------------------------------------------
+# Constants.
+pi2 = (2 * math.pi)
+ft_per_metre = 3.28084
+
+#------------------------------------------------------------------------------
+# Trig identities.
+
 def Cos2(angle):
     return math.pow(math.cos(angle), 2)
 
@@ -25,6 +33,9 @@ def Tan4(angle):
 def Tan5(angle):
     return math.pow(math.tan(angle), 5)
 
+#------------------------------------------------------------------------------
+# Miscellaneous.
+
 def DegToRad(degrees):
     """ Convert degrees to radians.
     """
@@ -41,79 +52,39 @@ def Circumference(radius):
     return 2 * math.pi * radius
 
 def ToSignedAngle(angle, degrees=True):
-    """ Convert an angle to the range -180 to 180.
+    """ Convert an angle to the range -180 to 180 (-pi to pi).
     """
-
-    # Degrees.
     if degrees:
         while angle > 180:
             angle = angle - 360
         while angle < -180:
             angle = angle + 360
+    else:
+        while angle > math.pi:
+            angle = angle - pi2
+        while angle < -math.pi:
+            angle = angle + pi2
 
     return angle
 
 def ToUnsignedAngle(angle, degrees=True):
-    """ Convert an angle to the range 0 to 360.
+    """ Convert an angle to the range 0 to 360 (0 to 2*pi).
     """
-
-    # Degrees.
     if degrees:
         while angle > 360:
             angle -= 360
         while angle < 0:
             angle += 360
+    else:
+        while angle > pi2:
+            angle -= pi2
+        while angle < 0:
+            angle += pi2
 
     return angle
 
-def DdToDms(dd, latitude=True, degrees=True):
-    """ Convert decimal degrees angle to degrees minutes seconds string.
-    """
+def MetresToFeet(metres):
+    return metres * ft_per_metre
 
-    # Work with positive numbers.
-    dd_temp = dd
-    if dd < 0:
-        dd_temp *= -1
-
-    # Calculate components.
-    degrees = math.floor(dd_temp)
-    minutes = math.floor((dd_temp - degrees) * 60)
-    seconds = (dd_temp - degrees - (minutes / 60)) * 3600
-    seconds = round(seconds, 3)
-
-    # Stringify degrees.
-    if latitude:
-        degrees_str = f"{degrees}".rjust(2, "0")
-    else:
-        degrees_str = f"{degrees}".rjust(3, "0")
-    
-    # Stringify minutes.
-    minutes_str = f"{minutes}".rjust(2, "0")
-
-    # Stringify seconds.
-    seconds_str_parts = f"{seconds}".split(".")
-    seconds_str = seconds_str_parts[0].rjust(2, "0") + "." + seconds_str_parts[1].ljust(3, "0")
-
-    # Full string.
-    dms_str = f"{degrees_str} {minutes_str} {seconds_str} "
-
-    # Correct N/S or E/W.
-    if dd > 0:
-        if latitude:
-            dms_str += "N"
-        else:
-            dms_str += "E"
-    else:
-        if latitude:
-            dms_str += "S"
-        else:
-            dms_str += "W"
-
-    return dms_str
-
-def LatLonDdToDms(lat, lon, degrees=True):
-    """ Convert a DD lat/lon pair to a DMS string.
-    """
-    dms_lat = DdToDms(lat, True)
-    dms_lon = DdToDms(lon, False)
-    return f"{dms_lat},{dms_lon}"
+def FeetToMetres(feet):
+    return feet / ft_per_metre
