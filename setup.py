799d42bf20a3865801275a0625bde730fc12e5c1
Nambarc <25438361+Nambarc@users.noreply.github.com>
2023-09-08 12:56:11 +0100

    Prepare for release 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cruntils",
-    version="0.0.3",
+    version="0.0.4",
     description="A collection of utilities, mainly for me.",
     author="Nambarc",
     packages=["cruntils"]
