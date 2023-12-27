4662b22aae3ce8b560ba55cd1c8d5e07b722824d
Nambarc <25438361+Nambarc@users.noreply.github.com>
2023-12-27 17:14:54 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cruntils",
-    version="0.0.9",
+    version="0.0.10",
     description="A collection of utilities, mainly for me.",
     author="Nambarc",
     packages=["cruntils"],
