017ccf605021c92870c367810986d24a28d78244
Nambarc <25438361+Nambarc@users.noreply.github.com>
2024-01-18 23:08:52 +0000

    Added dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="cruntils",
-    version="0.0.12",
+    version="0.0.13",
     description="A collection of utilities.",
     author="Nambarc",
     packages=["cruntils"],
@@ -10,6 +10,7 @@ setuptools.setup(
     package_data={"": ["EGM96_WW_15M_GH.GRD"]},
     install_requires=[
         "setuptools>=61.0",
-        "pyserial>=3.5"
+        "pyserial>=3.5",
+        "pywin32>=306"
     ]
 )
