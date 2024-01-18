f20eee5af4fe0dc0009d9e6520358c366dbeb37d
Nambarc <25438361+Nambarc@users.noreply.github.com>
2024-01-18 23:21:41 +0000

    Trying to add runtime dependencies

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="cruntils",
-    version="0.0.13",
+    version="0.0.14",
     description="A collection of utilities.",
     author="Nambarc",
     packages=["cruntils"],
     include_package_data=True,
     package_data={"": ["EGM96_WW_15M_GH.GRD"]},
-    install_requires=[
-        "setuptools>=61.0",
-        "pyserial>=3.5",
-        "pywin32>=306"
-    ]
+    install_requires=["setuptools>=61.0"]
 )
